2748be2530f219806074f25118c1b9dac246aa5d
luguanxing <651571925@qq.com>
2019-02-16 02:20:10 +0800

    使用Mutex进行同步加锁

---
+++ 09-管道Channel/05.传统同步机制.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type atomicInt struct {
+	value int
+	lock  sync.Mutex
+}
+
+func (a *atomicInt) incr() {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	a.value++
+}
+
+func (a *atomicInt) get() int {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	return a.value
+}
+
+func main() {
+	var a atomicInt
+	a.incr()
+	go func() {
+		a.incr()
+	}()
+	time.Sleep(time.Millisecond)
+	fmt.Println(a.value)
+}
